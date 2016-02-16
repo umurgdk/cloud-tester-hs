d0fdcbf26b0e64770be80310b26294b954046403
Umur Gedik <umurgdk@gmail.com>
2016-02-16 23:47:24 +0100

    add getContainer to docker api

--- src/DockerApi.hs
+++ src/DockerApi.hs
@@ -8,13 +8,17 @@
 
 module DockerApi where
 
-import           Control.Lens
+import           Control.Lens                hiding ((.=))
 import           Data.Aeson
+import           Data.Aeson.Lens             (key, _String)
 import           Data.Aeson.Types
-import           Data.ByteString.Lazy        (ByteString)
+import           Data.ByteString.Lazy        (ByteString, toStrict)
 import qualified Data.ByteString.Lazy.Char8  as B
 import           Data.Char
+import           Data.List                   (find)
+import           Data.Maybe
 import qualified Data.Text                   as T
+import           Data.Text.Encoding
 import           GHC.Generics
 
 import           Network.HTTP.Client.OpenSSL
@@ -73,11 +77,36 @@ buildRestURL host path = "https://" ++ host ++ ":2376" ++ path
 getJSON :: FromJSON a => String -> IO a
 getJSON url = withOpenSSL $ (^. responseBody) <$> (asJSON =<< getWith optionsWithSSL url)
 
-getText url = withOpenSSL $ (^. responseBody) <$> getWith optionsWithSSL url
+getText :: String -> IO T.Text
+getText url = withOpenSSL $ responseBodyAsText <$> getWith optionsWithSSL url
+    where responseBodyAsText = decodeUtf8 . toStrict . (^. responseBody)
+
+postJSON :: (FromJSON a, ToJSON b) => String -> b -> IO a
+postJSON url postData = withOpenSSL $ (^. responseBody) <$> (asJSON =<< postWith optionsWithSSL url (toJSON postData))
+
+postJSON_ :: ToJSON a => String -> a -> IO T.Text
+postJSON_ url postData = withOpenSSL $ responseBodyAsText <$> postWith optionsWithSSL url (toJSON postData)
+    where responseBodyAsText = decodeUtf8 . toStrict . (^. responseBody)
 
 getDockerInfo :: String -> IO DockerInfo
 getDockerInfo host = getJSON $ buildRestURL host "/info"
 
 getContainers :: String -> IO [Container]
 getContainers host = getJSON $ buildRestURL host "/containers/json?all=1"
+
+getContainer :: String -> String -> IO Container
+getContainer host id = getJSON $ buildRestURL host $ "/containers/" ++ id ++ "/json"
+
+createContainer :: String ->  String -> IO Container
+createContainer host imageName = do
+    response <- postJSON url createData
+    let (Just id) = parseMaybe (.: "Id") response :: Maybe T.Text
+
+    fromJust . find (finder id) <$> getContainers host
+
+    where url = buildRestURL host "/containers/create"
+          createData = object [ "Image" .= imageName ]
+          finder id' container = id' == (container ^. DockerApi.id)
+
+
 
